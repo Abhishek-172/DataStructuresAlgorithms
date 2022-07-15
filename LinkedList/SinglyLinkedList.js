fdc41e8bf6074b428c9754fdc905dc01a84a4e69
Abhishek Pandey <abhishek.pandey172@gmail.com>
2022-07-15 22:14:42 +0530

    Added New Methods to Singly Linked List

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -4,58 +4,124 @@ class Node {
         this.next = null;
     }
 }
- 
+
 class SinglyLinkedList {
     constructor() {
+        this.length = 0;
         this.head = null;
         this.tail = null;
-        this.length = 0;
     }
+    // Adds the new node
     push(val) {
         let newNode = new Node(val);
-        if(this.head == null) {
+        // Edge Case
+        if(!this.head) {
             this.head = newNode;
             this.tail = newNode;
-            this.length = this.length+1;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-            this.length = this.length+1;
         }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length = this.length + 1;   
+        // Return the Newly Created SinglyLinkedList
         return this;
     }
+    // Removes the last node from SinglyLinkedList
     pop() {
-        if (!this.head) return undefined;
+        if(!this.head) {
+            return undefined;
+        }
         let current = this.head;
         let newTail = current;
-        while (current.next) {
+        while(current.next) {
             newTail = current;
             current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        // Edge Case
-        if (this.length === 0) {
+        if(this.length === 0) {
             this.head = null;
             this.tail = null;
         }
+        // Return the Deleted Node
         return current;
     }
+    // Removes the First Node From the Linked list
     shift() {
-        if (!this.head) return undefined;
+        if(!this.head) {
+            return undefined;
+        }
         let current = this.head;
         this.head = current.next;
         this.length--;
-        return this.current.val;
+        if(this.length === 0) {
+            this.tail = null;
+        }
+        // Return the Deleted Node Value
+        return current;
+    }
+    // Add New Node at the beginning
+    unshift(val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;   
+        }
+        this.length++;
+        return this;
+    }
+    // Get
+    get(index) {
+        if(index < 0 || index >= this.length) {
+            return 'Please provide correct index Value';
+        } else {
+            let count = 0;
+            let current = this.head;
+            while(count!==index) {
+                current = current.next;
+                count++;
+            }
+            return current;
+        }
+    }
+    // Set 
+    set(index, newVal) {
+        let foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.val = newVal;
+            return true;
+        }
+        return false; 
+      }
+    // Insert
+    insert(index, newVal) {
+        if(index < 0 || index > this.length) {
+            return false;
+        }
+        else if(index === this.length) {
+            return this.push(newVal);            
+        }
+        else if(index === 0) {
+            return this.unshift(newVal);
+        }
+        else {
+            let newNode = new Node(newVal);
+            let foundNode = this.get(index-1);
+            let previousNode = foundNode.next;
+            foundNode.next = newNode;
+            newNode.next = previousNode;
+            this.length++;
+            return true;
+        }
     }
 }
 
 let list = new SinglyLinkedList();
-list.push('Abhishek Pandey');
-list.push('Divya Pandey');
-list.push('Padma Pandey');
-list.push('Ravi Uday Pandey');
-list.pop();
-list.shift()
-console.log('I am the List:::', list);
+list.push('Amazon');
+list.push('Apple');
+list.push('Atlassian');
+list.push('Google');
+list.push('GoldmanSachs');
