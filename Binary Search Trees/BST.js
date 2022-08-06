b7b8b61eee2652857b08936f64ff7bb0587fa836
Abhishek Pandey <abhishek.pandey172@gmail.com>
2022-08-06 10:27:36 +0530

    Added Traversal Techniques

--- Binary Search Trees/BST.js
+++ Binary Search Trees/BST.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(value) {
+    constructor(value){
         this.value = value;
         this.left = null;
         this.right = null;
@@ -11,63 +11,123 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value) {
-        let newNode = new Node(10);
-        if (!this.root) {
+        var newNode = new Node(value);
+        if (this.root === null) {
             this.root = newNode;
             return this;
         }
-        else {
-            let current = this.root;
-            while (true) {
-                if (value === current.value) {
-                    return undefined;
+        var current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
                 }
-                if (value < current.value) {
-                    if (current.left === null) {
-                        current.left = value;
-                        return this;
-                    }
-                    else {
-                        current = current.left;
-                    }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
                 }
-                else if (value > current.value) {
-                    if (current.right === null) {
-                        current.right = value;
-                        return this;
-                    }
-                    else {
-                        current = current.right;
-                    }
-                }    
+                current = current.right;
             }
         }
     }
-    found(value) {
-        if (this.root === null) {
-            return false;
-        }
-        let current = this.root;
-        let found = false;
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
         while (current && !found) {
-            if (value > current.value) {
-                current = current.right;
-            }
-            else if (value < current.value) {
+            if (value < current.value) {
                 current = current.left;
-            }
-            else {
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
                 found = true;
             }
         }
-        if (!found) return 'Not Found';
+        if (!found) return undefined;
         return current;
+    }
+    contains(value) {
+        if (this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    BFS() {
+        // Create a Arrays, 1. Data 2. Queue
+        let node = this.root;
+        let data = [] , queue = [];
+        // Add Root Node in Queue Array
+        queue.push(node);
+        // Loop As long as there is something in the queue
+        while (queue.length) {
+            // Take out the first element from the Queue
+            node = queue.shift();
+            // Now add this node to the data Array
+            data.push(node);
+            // Now check if the node have left or right, if yes, add it to the Queue
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+        return data;
+    }
+    DFSPreOrder() {
+        // Recursive Way
+        let data = [];
+        function traversal(node) {
+            data.push(node.value);
+            if (node.left) { traversal(node.left); }
+            if (node.right) { traversal(node.right);}
+        }
+        traversal(this.root);
+        return data;
+    }
+    DSFPostOrder() {
+        // Recursive Way
+        let data = [];
+        function traversal(node) {
+            if (node.left) { traversal(node.left); }
+            if (node.right) { traversal(node.right); }
+            data.push(node);
+        }
+        traversal(this.root);
+    }
+    DFSInorder() {
+        let data = [];
+        function traversal(node) {
+            if (node.left) { traversal(node.left); }
+            data.push(node);
+            if (node.right) { traversal(node.right); }
+        }   
+        traversal(this.root);
+        return data;
     }
 }
 
-let tree = new BinarySearchTree();
-tree.insert(1);
-tree.insert(2);
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
 tree.insert(3);
-tree.insert(4);
-tree.insert(5);
+tree.insert(8);
+tree.insert(20);
+tree.BFS();
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInorder();
---
+++ Tree Traversal/Introduction.js
@@ -0,0 +1,29 @@
+/*
+There are two types of Traversals:
+1. Breadth First Traversal
+2. Depth First Traversal
+These refer to general directions
+BFS goes from Left to Right
+
+BFS::::::
+          N1
+    N2          N3
+N4      N5   N6     N7   
+Go from Left to right
+Start with Root - N1
+Then from N2 to N3
+Then from N4 to N7
+------->
+
+DFS::::::
+It goes from Top to Bottom
+And it have 3 types:
+-> Inorder
+-> PreOrder
+-> PostOrder
+
+
+Whats Left?
+-> Which one is better DFS or BFS?
+-> Out of Inorder, Preorder and Postorder which one is better?
+*/
